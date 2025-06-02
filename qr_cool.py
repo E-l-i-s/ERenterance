2defd9a405297151f85c24c2696db569ae634b15
E-l-i-s <elisabedizhgenti@gmail.com>
2025-06-02 22:17:12 +0200

    Update qr_cool.py

--- qr_cool.py
+++ qr_cool.py
@@ -1,10 +1,17 @@
 import qrcode
 
 def show_qr():
+    """
+    Generates and prints an ASCII QR code that links to a YouTube video.
+    """
     url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    # Create a QRCode object with a small border
     qr = qrcode.QRCode(border=1)
+    # Add the URL data to the QRCode
     qr.add_data(url)
+    # Generate the QR code, adjusting size to fit the data
     qr.make(fit=True)
+    # Print the QR code as ASCII characters in the terminal
     qr.print_ascii(invert=True)
     print("\nScan this QR code with your phone for the surprise!")
 
